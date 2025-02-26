50dc70470fe7992217af703735c8986d1d9c578f
yukicxz <133191761+yukicxz@users.noreply.github.com>
2025-02-26 19:50:15 +0300

    add 1st lab

---
+++ lab1/src/main/java/com/mycompany/lab1/Lab1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author Александра
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
